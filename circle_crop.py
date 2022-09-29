0bda7f1763dfba3213c29323bd8ddc85acb7d82c
BinaryByte <vishal01mehra@gmail.com>
2022-09-30 02:32:35 +0530

    Update circle_crop.py

--- circle_crop.py
+++ circle_crop.py
@@ -3,7 +3,6 @@ import numpy as np
 from PIL import Image, ImageDraw
   
 tempImg=Image.open("image.jpg")
-#display(img)
 left_1 = 500
 top_1 = 30
 right_1 = 3350
@@ -19,7 +18,9 @@ draw.pieslice([(50,70), (height,width)], 0, 360,
               fill = 255, outline = "white")
 img_arr =np.array(img)
 lum_img_arr =np.array(lum_img)
-#display(Image.fromarray(lum_img_arr))
 final_img_arr = np.dstack((img_arr,lum_img_arr))
-finalPhoto = Image.fromarray(final_img_arr)
-finalPhoto.save("output.png")
+photo = Image.fromarray(final_img_arr)
+finalPhoto = photo.convert('RGB")
+finalPhoto.save('output.jpg',
+               format = 'JPEG',
+                 quality = 100)
